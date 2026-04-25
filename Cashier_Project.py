00f5013a73de3eb88cc55e9b34551e23314d3c38
Cheesecake <kani.kengatharan@datawizpro.com>
2026-04-25 10:30:38 -0400

    not enough money

--- Cashier_Project.py
+++ Cashier_Project.py
@@ -1,4 +1,8 @@
+import sys
 user_cash = int(input("What is the cost of your item? "))
 
 user_spent = int(input("How much are you giving to spend? "))
 
+if user_spent < user_cash:
+    print("Not enough money. Sorry you can't purchase this item. BYEEEE")
+    sys.exit()
